71cb7ed3a07e44f84a83c107d5bdc0c563ee2ba6
Matt Cale <mattc41190@gmail.com>
2019-01-22 08:03:56 -0600

    01-02-009-switch

--- 01-basics/02-flow-control/008-flow-exercise/main.go
+++ 01-basics/02-flow-control/008-flow-exercise/main.go
@@ -5,14 +5,31 @@ import(
 )
 
 func Sqrt(x float64) float64 {
-	guess := 1.0
+	var prevGuess float64
+	var guess = 1.0
 	for i := 0; i < 10; i++ {
+		if prevGuess == guess {
+			return guess
+		}
+		prevGuess = guess
 		guess -= (guess*guess - x) / (2*guess)
-		fmt.Println(guess)
 	}
 	return guess
 }
 
 func main() {
-	Sqrt(4)
+	fmt.Println(Sqrt(4))
 }
+
+// First Loop: (Assuming 4.0 passed in)
+// 1 x 1 - 4 = -3
+// 2 * 1 = 2
+// -3 / 2 = -1.5
+// 1 - -1.5 = 2.5
+
+// Second Loop:
+// 2.5 * 2.5 - 4 = 2.25
+// 2 * 2.5 = 5
+// 2.25 / 5 = .45
+// 2.5 - 0.45 = 2.05
+// ...
